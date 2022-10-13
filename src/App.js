fa2b1044c886f2f0dfe02190e02ae7030534b65b
johannes <johannes.muljana@ait.nsw.edu.au>
2022-10-14 10:15:42 +1100

    adding content to app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">App</a>
+        </div>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <h3>Column 1</h3>
+          </div>
+          <div className="col">
+            <h3>Column 2</h3>
+          </div>
+          <div className="col">
+            <h3>Column 3</h3>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className="container-fluid">
+          <p>Footer</p>
+        </div>
+      </footer>
     </div>
   );
 }
