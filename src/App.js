1b9b8ab97e445e44491085c0223af66b36833fba
johannes <johannes.muljana@ait.nsw.edu.au>
2022-10-21 10:42:54 +1100

    adding Routes and error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {Header} from './components/Header'
 import {Footer} from './components/Footer'
+import { Routes, Route, Outlet, Link  } from 'react-router-dom';
+// import page components
+import { Home } from './pages/Home'
+import { Contact } from './pages/Contact'
+import { About } from './pages/About'
 
 const NavData = [
   { name: "Home", path: "/", public: true },
@@ -13,19 +18,11 @@ function App() {
   return (
     <div className="App">
       <Header title="My app" headernav={ NavData } />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <h3>Column 1</h3>
-          </div>
-          <div className="col">
-            <h3>Column 2</h3>
-          </div>
-          <div className="col">
-            <h3>Column 3</h3>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/contact" element={ <Contact /> } />
+      </Routes>
       <Footer year="2022" />
     </div>
   );
