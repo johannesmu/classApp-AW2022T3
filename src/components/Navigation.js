a34b8c2ed6781d9b3347c92d08143905b4cea3c0
johannes <johannes.muljana@ait.nsw.edu.au>
2022-10-21 09:27:17 +1100

    added navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,9 @@
+export function Navigation ( props ) {
+    return (
+        <nav className="navbar-nav me-auto mb-2 mb-lg-0">
+            <a href="#" className="nav-link">Home</a>
+            <a href="#" className="nav-link">About</a>
+            <a href="#" className="nav-link">Contact</a>
+        </nav>
+    )
+}
