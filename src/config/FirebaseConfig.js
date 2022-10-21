d1ac104fad69a1a64b870dc867cbb3316e5a5916
johannes <johannes.muljana@ait.nsw.edu.au>
2022-10-21 11:10:21 +1100

    adding firebase, config and firebase-tools

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyDb_qKGq_zy1cWCbX1ugCXPGoU3aTTknMk",
+    authDomain: "bookreview-e6567.firebaseapp.com",
+    projectId: "bookreview-e6567",
+    storageBucket: "bookreview-e6567.appspot.com",
+    messagingSenderId: "692425642650",
+    appId: "1:692425642650:web:df90ae10a0d301e50c0523"
+};
