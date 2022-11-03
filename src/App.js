80f61dda74dd68c06d225f4188e7072bef775476
johannes <johannes.muljana@ait.nsw.edu.au>
2022-11-04 10:47:44 +1100

    added redirection to home after auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
-import {Header} from './components/Header'
-import {Footer} from './components/Footer'
-import { Routes, Route  } from 'react-router-dom';
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import { Routes, Route } from 'react-router-dom';
 // import page components
 import { Home } from './pages/Home'
 import { Contact } from './pages/Contact'
@@ -13,22 +13,23 @@ import { Signup } from './pages/Signup'
 import { initializeApp } from "firebase/app"
 import { FirebaseConfig } from './config/FirebaseConfig'
 // import firebase auth
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth"
 
 // initialise Firebase
 const FBapp = initializeApp(FirebaseConfig)
 // initialise Firebase Auth
-const FBauth = getAuth( FBapp )
+const FBauth = getAuth(FBapp)
+
 
 // function to create user account
-const signup = ( email, password ) => {
-  return new Promise( ( resolve, reject ) => {
-    createUserWithEmailAndPassword( FBauth, email, password )
-    .then( (userCredential) => resolve(userCredential.user) )
-    .catch( (error) =>{
-      console.log( error )
-      reject(error)
-    })
+const signup = (email, password) => {
+  return new Promise((resolve, reject) => {
+    createUserWithEmailAndPassword(FBauth, email, password)
+      .then((userCredential) => resolve(userCredential.user))
+      .catch((error) => {
+        // console.log(error)
+        reject(error)
+      })
   })
 }
 
@@ -40,14 +41,30 @@ const NavData = [
 ]
 
 function App() {
+  const [auth, setAuth] = useState()
+
+  // an observer to determine user's authentication status
+  onAuthStateChanged(FBauth, (user) => {
+    if (user) {
+      // visitor is authenticated
+      console.log( user )
+      setAuth( user )
+    }
+    else {
+      // if user is null means visitor is not authenticated
+      console.log( 'not signed in' )
+      setAuth( null )
+    }
+  })
+
   return (
     <div className="App">
-      <Header title="My app" headernav={ NavData } />
+      <Header title="My app" headernav={NavData} />
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
-        <Route path="/contact" element={ <Contact /> } />
-        <Route path="/signup" element={ <Signup handler={signup} /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup handler={signup} />} />
       </Routes>
       <Footer year="2022" />
     </div>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,84 +1,108 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export function Signup ( props ) {
-    const [ email, setEmail ] = useState('')
-    const [ password, setPassword ] = useState('')
-    const [ validEmail, setValidEmail ] = useState(false)
-    const [ validPassword, setValidPassword ] = useState(false)
+export function Signup(props) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [validEmail, setValidEmail] = useState(false)
+  const [validPassword, setValidPassword] = useState(false)
+  const [error, setError] = useState()
+  const [success, setSuccess] = useState(false)
 
-    useEffect( () => { 
-        // check the value of email
-        // check if email contains @ and it's not the first character
-        if( email.length >= 5 && email.indexOf('@') > 0 ) {
-            setValidEmail( true )
-        }
-        else {
-            setValidEmail( false )
-        }
-     }, [email] )
-
-     useEffect( () => {
-        if( password.length >= 8 ) {
-            setValidPassword( true )
-        }
-        else {
-            setValidPassword( false )
-        }
-     }, [password])
-
-    const submitHandler = ( event ) => {
-        console.log('submit')
-        // stop the form from refreshing the page
-        event.preventDefault()
-        // capture data from form
-        const data = new FormData(event.target)
-        props.handler( data.get("useremail"), data.get("userpw") )
-        .then(() => console.log('success') )
-        .catch((error) => {
-            //console.log(error)
-        })
+  useEffect(() => {
+    // check the value of email
+    // check if email contains @ and it's not the first character
+    if (email.length >= 5 && email.indexOf('@') > 0) {
+      setValidEmail(true)
     }
+    else {
+      setValidEmail(false)
+    }
+  }, [email])
 
-    return (
-        <div className="container">
-            <div className="row">
-                <form className="col-md-4 offset-md-4" onSubmit={ submitHandler }>
-                    <h2>Sign up for an account</h2>
-                    <div className="mb-3">
-                        <label htmlFor="useremail">Email (valid email address) </label>
-                        <input 
-                            type="email" 
-                            id="useremail" 
-                            name="useremail"
-                            placeholder="you@domain.com" 
-                            className="form-control" 
-                            value={ email }
-                            onChange={ (event) => setEmail(event.target.value) }
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="userpw">Password (minimum 8 characters)</label>
-                        <input 
-                            type="password" 
-                            id="userpw" 
-                            name="userpw"
-                            placeholder="you@domain.com" 
-                            className="form-control" 
-                            value={password}
-                            onChange={ (event) => setPassword(event.target.value) }
-                        />
-                    </div>
-                    <div className="d-grid">
-                        <button 
-                            type="submit" 
-                            className="btn btn-primary"
-                            disabled = { ( validEmail && validPassword ) ? false : true }
-                        >
-                            Sign up
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    if (password.length >= 8) {
+      setValidPassword(true)
+    }
+    else {
+      setValidPassword(false)
+    }
+  }, [password])
+
+  useEffect( () => {
+    if(success) {
+      navigate('/')
+    }
+  }, [success])
+
+  const submitHandler = (event) => {
+    // console.log('submit')
+    // stop the form from refreshing the page
+    event.preventDefault()
+    // reset error message
+    setError(null)
+    // capture data from form
+    const data = new FormData(event.target)
+    props.handler(data.get("useremail"), data.get("userpw"))
+      .then(() => setSuccess(true))
+      .catch((error) => {
+        //console.log(error)
+        handleErrors(error.code)
+      })
+  }
+
+  const handleErrors = (code) => {
+    // turn firebase errors into plain error messages for the visitor
+    if ("auth/email-already-in-use") {
+      setError("the email is already in use")
+    }
+  }
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <div className="row">
+        <form className="col-md-4 offset-md-4" onSubmit={submitHandler}>
+          <h2>Sign up for an account</h2>
+          <div className="mb-3">
+            <label htmlFor="useremail">Email (valid email address) </label>
+            <input
+              type="email"
+              id="useremail"
+              name="useremail"
+              placeholder="you@domain.com"
+              className="form-control"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="userpw">Password (minimum 8 characters)</label>
+            <input
+              type="password"
+              id="userpw"
+              name="userpw"
+              placeholder="you@domain.com"
+              className="form-control"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="d-grid">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={(validEmail && validPassword) ? false : true}
+            >
+              Sign up
+            </button>
+            <div>{error}</div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+
+
 }
