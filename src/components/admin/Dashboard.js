b225e7c1f7374ace090b7bc67f6c532ac7c29476
johannes <jmuljana@gmail.com>
2022-11-23 19:56:01 +1100

    added dashboard

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -1,8 +1,11 @@
+import 
 export function Dashboard( props ) {
   return (
     <div className="container">
       <div className="row">
-        
+        <div className="col">
+
+        </div>
       </div>
     </div>
   )
