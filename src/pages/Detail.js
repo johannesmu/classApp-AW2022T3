92c57764afb51a238b219b4ea64e0071edbac001
johannes <johannes.muljana@ait.nsw.edu.au>
2022-11-25 10:59:49 +1100

    adding username in signup and showing more details

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -11,6 +11,8 @@ export function Detail ( props ) {
             props.getter("books", bookId)
             .then( (data) => {
                 setBookData(data)
+                // book image
+                
             } )
         }
     })
@@ -22,18 +24,28 @@ export function Detail ( props ) {
             <div className="row">
                 <div className="col">
                     <h2>{ bookData.Title }</h2>
+                    <h4>{ bookData.Tagline }</h4>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <h2>{bookId}</h2>
-                    <h3>book cover image</h3>
+                    <DetailImage data={bookData} getter={props.imageGetter} />
                 </div>
                 <div className="col">
-                    <h3>ISBN</h3>
-                    <h3>Summary</h3>
-                    <button className="btn btn-info">Add to Favouries</button>
-                    <button className="btn btn-info">Review this book</button>
+                    <h4>Author</h4>
+                    <p>{bookData.Author}</p>
+                    <h5>ISBN 10</h5>
+                    <p>{bookData.ISBN10 }</p>
+                    <h5>ISBN 13</h5>
+                    <p>{bookData.ISBN13 }</p>
+                    <h5>Pages</h5>
+                    <p>{bookData.Pages }</p>
+                    <h5>Year</h5>
+                    <p>{bookData.Year }</p>
+                    <div style={ (props.auth) ? { display: "block"} : { display: "none"} }>
+                        <button className="btn btn-info">Add to Favouries</button>
+                        <button className="btn btn-info">Review this book</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -43,5 +55,25 @@ export function Detail ( props ) {
         return(
             <div className="container my-4"></div>
         )
+    }
+}
+
+function DetailImage( props ) {
+    const[ imgUrl, setImgUrl ] = useState()
+
+    useEffect( () => {
+       if( props.data ) {
+          props.getter("book_covers/" + props.data.Cover )
+          .then( (url) => setImgUrl(url) )
+       }
+    }, [props.data ])
+
+    if( imgUrl ) {
+        return (
+            <img src={imgUrl} style={{ width: "100%"}} />
+        )
+    }
+    else {
+        return <p>Loading...</p>
     }
 }
